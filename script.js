2deb7b0d547eea001a76db517c83256a140bceb4
Thomas Auger_1G3 <thomas.auger2009@gmail.com>
2026-02-12 16:37:46 +0100

    javascripttttttttttttt

---
+++ script.js
@@ -0,0 +1,20 @@
+const jsonFR = {
+  "sommaire1": "Sommaire 1G3"
+}
+const jsonEN = {
+  "sommaire1": "1G3's Table of content"
+}
+
+document.querySelector('.language').addEventListener("change",function(){
+  if (this.value == "french"){
+    for (let key in jsonFR){
+      let element = "." + key
+      document.querySelector(element).textContent = jsonFR[key]
+    }
+  } else if (this.value == "english") {
+   for (let  key in jsonEN){
+      let element = "." + key
+      document.querySelector(element).textContent = jsonEN[key]
+    }
+  }
+})
