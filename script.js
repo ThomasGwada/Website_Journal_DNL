09b9f6745ba2adb3205f4278bf5f2f8478d89a42
GrandChtoun <thomas.auger2009@gmail.com>
2026-03-26 16:34:39 +0100

    J'aime le Jambon
    j'aime bien notre Web designer il est trop sympa :3

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const jsonFR = {
   "sommaire1": "Sommaire 1G3 :",
-  "sommaire2": "Sommaire 1G4 :",
   "article3_1": "Article 1",
   "article3_2": "Article 2",
   "article3_3": "Article 3",
 }
 const jsonEN = {
   "sommaire1": "1G3's Table of content :",
-  "sommaire2": "1G4's Table of content :",
   "article3_1": "Article 1 (english)",
   "article3_2": "Article 2 (english)",
   "article3_3": "Article 3 (english)",
@@ -21,15 +19,11 @@ document.querySelector('.language').addEventListener("change",function(){
       document.querySelector(element).textContent = jsonFR[key]
       console.log("la france")
     }
-    selector.classList.add("drapeau_france")
-    selector.classList.remove("drapeau_anglais")
   } else if (this.value == "english") {
    for (let  key in jsonEN){
       let element = "." + key
       document.querySelector(element).textContent = jsonEN[key]
       console.log("anglais")
     }
-    selector.classList.add("drapeau_anglais")
-    selector.classList.remove("drapeau_france")
   }
 })
